583af750ee16f0056517736cf0ef6831ca5bdff8
DEX <134099125+davidt369@users.noreply.github.com>
2024-11-11 12:00:58 -0400

    Actualizar firebase.ts

--- app/config/firebase.ts
+++ app/config/firebase.ts
@@ -9,15 +9,7 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCqDMX7vQy4nAAEWmqGPvIBVuMCi_BKNHg",
-  authDomain: "ecomerce-b18d2.firebaseapp.com",
-  projectId: "ecomerce-b18d2",
-  storageBucket: "ecomerce-b18d2.appspot.com",
-  messagingSenderId: "964782725340",
-  appId: "1:964782725340:web:896b52ffc7c4df6ac57c19",
-  measurementId: "G-JXK5BJYTSE"
-};
+
 
 // Inicializa Firebase
 export const app = initializeApp(firebaseConfig);
